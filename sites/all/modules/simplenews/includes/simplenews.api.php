0724f3d5851553a70c1fe1a27ced3d8ae9919fa8
Mikhail V <soonreal@gmail.com>
2012-05-09 18:53:34 +0400

    the first upload to master branch

---
+++ sites/all/modules/simplenews/includes/simplenews.api.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Simplenews module.
+ */
+
+/**
+ * @todo
+ */
+function hook_simplenews_issue_operations() {
+
+}
+
+/**
+ * @todo
+ */
+function hook_simplenews_subscription_operations() {
+
+}
+
+/**
+ * @todo
+ */
+function hook_simplenews_category_insert($category) {
+
+}
+
+/**
+ * @todo
+ */
+function hook_simplenews_category_update($category) {
+
+}
+
+/**
+ * @todo
+ */
+function hook_simplenews_category_delete($category) {
+
+}
+
+/**
+ * @todo
+ */
+function hook_simplenews_mailing_list_insert($list) {
+
+}
+
+/**
+ * @todo
+ */
+function hook_simplenews_subscriber_update($subscriber) {
+
+}
+
+/**
+ * @todo
+ */
+function hook_simplenews_subscriber_insert($subscriber) {
+
+}
+
+/**
+ * @todo
+ */
+function hook_simplenews_subscriber_delete($subscriber) {
+
+}
+
+/**
+ * Invoked if a user is subscribed to a newsletter.
+ *
+ * @param $subscriber
+ *   The subscriber object including all subscriptions of this user.
+ *
+ * @param $subscription
+ *   The subscription object for this specific subscribe action.
+ */
+function hook_simplenews_subscribe_user($subscriber, $subscription) {
+
+}
+
+/**
+ * Invoked if a user is unsubscribed from a newsletter.
+ *
+ * @param $subscriber
+ *   The subscriber object including all subscriptions of this user.
+ *
+ * @param $subscription
+ *   The subscription object for this specific unsubscribe action.
+ */
+function hook_simplenews_unsubscribe_user($subscriber, $subscription) {
+
+}
