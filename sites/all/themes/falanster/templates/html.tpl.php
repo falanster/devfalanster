3348553a5a8297209b5188b28503cfe5b150b3bc
admin <soonreal@gmail.com>
2012-09-05 09:57:22 +0300

    cange html tpl

--- sites/all/themes/falanster/templates/html.tpl.php
+++ sites/all/themes/falanster/templates/html.tpl.php
@@ -2,7 +2,13 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
 <head<?php print $rdf->profile; ?>>
   <?php print $head; ?>
-  <title><?php print $head_title; ?></title>  
+  <title>  
+  <?php 
+  if (empty($head_title_array['title'])) {
+    print t($head_title_array['name']);
+  } else {
+    print $head_title;
+  }?></title>  
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
