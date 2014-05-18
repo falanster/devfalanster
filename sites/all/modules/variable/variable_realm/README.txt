8901a7dad0bea434920016dec68e5debc405ad6a
falanster-srv <falanster.by@gmail.com>
2014-05-18 13:46:59 +0300

    up to 7.28 + up modules

--- sites/all/modules/variable/variable_realm/README.txt
+++ sites/all/modules/variable/variable_realm/README.txt
@@ -53,3 +53,41 @@ This is an example of how realms work:
   variable_realm_switch('nodetype', 'story')
 
 An example of a module using this API is Internationalization's i18n_variable module.
+
+Variable Realm Union.
+====================================
+
+This an API that allows combining two existing realms into a new one
+whose keys are a combination of the other two.
+
+An example of this module in action is the 'Domain+I18n Variables Integration' module
+which is part of 'Domain Variable' module.
+
+How to use it.
+=============
+To define a new domain that is a combination of two or more existing ones:
+
+1. Implement hook_variable_realm_info() to define the realm name and properties.
+
+function domain_i18n_variable_variable_realm_info() {
+  $realm['domain_language'] = array(
+    'title' => t('Domain+Language'),
+    // Display on settings forms but without form switcher.
+    'form settings' => TRUE,
+    'form switcher' => FALSE,
+    'variable name' => t('multilingual domain'),
+  );
+  return $realm;
+}
+
+2. Implement hook_variable_realm_controller() to define the Controller class to
+    be used and which other realms it is a combination of. Example:
+
+function domain_i18n_variable_variable_realm_controller() {
+  $realm['domain_language'] = array(
+    'weight' => 200,
+    'class' => 'VariableStoreRealmController',
+    'union' => array('domain', 'language'),
+  );
+  return $realm;
+}
--- sites/all/modules/variable/variable_realm/variable_realm.api.php
+++ sites/all/modules/variable/variable_realm/variable_realm.api.php
@@ -10,31 +10,8 @@
  */
 
 /**
- * Provides information about controller classes and weights for variable realms.
- *
- * This information is mainly used to build exportable realms as Features though
- * it may be used in the future to dinamically load realms upon request. For this
- * reason this may be defined in the main module and available at bootstrap time
- * when the realm is going to be used from some module hook_boot(). For this, the
- * module may need to implement any of the bootstrap hooks like hook_boot().
- *
- * @see i18n_variable_variable_realm_controller()
- *
- * @return array
- *   Associative array keyed by realm name. Each element is an array containing:
- *   - 'class', Class name implementing RealmControllerInterface.
- *   - 'weight', Default weight for this realm.
- */
-function hook_variable_realm_controller() {
-  $realm['language'] = array(
-    'weight' => 100,
-    'class' => 'VariableStoreRealmController',
-  );
-  return $realm;
-}
-
-/**
- * Provides human readable information about existing realms.
+ * Provides information about controller classes and weights needed by the realm system
+ * and some more meta-information for realm administration.
  *
  * This information is mainly used to build exportable realms as Features.
  *
@@ -43,16 +20,77 @@ function hook_variable_realm_controller() {
  * @return array
  *   Array keyed by realm name which contains the following elements:
  *   - 'title', Humam readable name for the realm.
+ *   - 'controller class', Class name for realm controller, implementing VariableRealmControllerInterface.
+ *   - 'store class', Class name for realm store, implementing VariableRealmStoreInterface.
+ *   - 'weight', Default weight for this realm.
  *   - 'keys', Associative array with human readable names for keys.
+ *   - 'keys callback', Callback function to provide the keys.
+ *   - 'default key', The default key.
+ *   - 'options', Array of variable names that may be set for this realm. If not set
+ *     any variable will be allowed for this realm.
+ *   - 'list callback', Callback function to provide variable list for this realm.
+ *   - 'select', Boolean flag whether variables for this realm can be selected from a list.
+ *   - 'select path', Path to variable selection form (optional).
+ *   - 'variable name', Name for variables that belong to this realm: e.g. 'multilingual' variable/s
+ *   - 'variable class', CSS class name for annotated variables in system settings forms.
+ *   - 'form settings', Boolean flag, whether realm variables should be handled automatically
+ *     in system settings forms.
+ *   - 'form switcher', Boolean flag, whether a realm switcher should be auto-generated
+ *     for settings forms which contain variables that belong to this realm.
  */
 function hook_variable_realm_info() {
   $realm['language'] = array(
     'title' => t('Language'),
+    'controller class' => 'I18nVariableLanguageRealm',
+    'store class' => 'VariableStoreRealmStore',
     'keys' => locale_language_list('name', TRUE),
+    'default key' => language_default('language'),
+    'options' => _i18n_variable_variable_realm_list(),
+    'select' => TRUE,
+    'select path' => 'admin/config/regional/i18n/variable',
+    'variable name' => t('multilingual'),
+    'variable class' => 'i18n-variable',
+    'form settings' => TRUE,
   );
   return $realm;
 }
 
+/**
+ * Allow other modules to act when enabling a realm.
+ *
+ * This hook is invoked right after the realm controller is enabled
+ * and it may have a valid key already set or it may be FALSE.
+ *
+ * @param $realm_name
+ *   Realm that is switched.
+ * @param $realm_key
+ *   New realm key.
+ */
+function hook_variable_realm_enable($realm_name, $realm_key) {
+}
+
+/**
+ * Allow other modules to act on realm switching.
+ *
+ * This hook is invoked right after the realm key is switched but before
+ * the global variables are rebuilt.
+ *
+ * @param $realm_name
+ *   Realm that is switched.
+ * @param $realm_key
+ *   New realm key.
+ */
+function hook_variable_realm_switch($realm_name, $realm_key) {
+}
+
+/**
+ * Allow other modules to act when rebuilding the configuration.
+ *
+ * This hook is invoked before the global variables are rebuilt
+ * using the active realms.
+ */
+function hook_variable_realm_rebuild() {
+}
 /**
  * @} End of "addtogroup hooks".
  */
