b3fd63bbd576f2101b9fc4a7c4daf0dac8331d89
Test2#1090;Test2#1101;Test2#1089;Test2#1090;Test2#1072;Test2#1074;Test2#1099; Test2#1089;Test2#1077;Test2#1088;Test2#1074;Test2#1072;Test2#1082; Test2#1076;Test2#1083;Test2#1103; Test2#1092;Test2#1072;Test2#1083;Test2#1072;Test2#1085;Test2#1089;Test2#1090;Test2#1101;Test2#1088;Test2#1072 <test2@1171.s.time4vps.eu>
2015-07-18 14:11:17 +0300

    minor update to 7.38

--- sites/all/themes/adminimal_theme/templates/page--admin--appearance.tpl.php
+++ sites/all/themes/adminimal_theme/templates/page--admin--appearance.tpl.php
@@ -17,14 +17,21 @@
 
 	<?php print render($title_suffix); ?>
 
-	<?php print render($primary_local_tasks); ?>
+</div>
+
+<div id="navigation">
+
+  <?php if ($primary_local_tasks): ?>
+    <?php print render($primary_local_tasks); ?>
+  <?php endif; ?>
+
+  <?php if ($secondary_local_tasks): ?>
+    <div class="tabs-secondary clearfix"><ul class="tabs secondary"><?php print render($secondary_local_tasks); ?></ul></div>
+  <?php endif; ?>
 
 </div>
 
 <div id="page">
-	<?php if ($secondary_local_tasks): ?>
-		<div class="tabs-secondary clearfix"><ul class="tabs secondary"><?php print render($secondary_local_tasks); ?></ul></div>
-	<?php endif; ?>
 
 	<?php
 		// Get enabled themes.
@@ -68,7 +75,7 @@
     <?php endif; ?>
 
     <div id="main-content">
-      <?php print render($page['content']['system_main']); ?>
+      <?php print render($page['content']); ?>
     </div>
 
     <?php if (isset($page['sidebar_right'])): ?>
--- sites/all/themes/adminimal_theme/templates/page--admin--dashboard.tpl.php
+++ sites/all/themes/adminimal_theme/templates/page--admin--dashboard.tpl.php
@@ -17,14 +17,21 @@
 
 	<?php print render($title_suffix); ?>
 
-	<?php print render($primary_local_tasks); ?>
+</div>
+
+<div id="navigation">
+
+  <?php if ($primary_local_tasks): ?>
+    <?php print render($primary_local_tasks); ?>
+  <?php endif; ?>
+
+  <?php if ($secondary_local_tasks): ?>
+    <div class="tabs-secondary clearfix"><ul class="tabs secondary"><?php print render($secondary_local_tasks); ?></ul></div>
+  <?php endif; ?>
 
 </div>
 
 <div id="page">
-	<?php if ($secondary_local_tasks): ?>
-		<div class="tabs-secondary clearfix"><ul class="tabs secondary"><?php print render($secondary_local_tasks); ?></ul></div>
-	<?php endif; ?>
 
 	<div id="content" class="clearfix">
 		<div class="element-invisible"><a id="main-content"></a></div>
@@ -56,14 +63,7 @@
     <?php endif; ?>
 
     <div id="main-content">
-
-      <?php if (isset($page['content']['dashboard'])): ?>
-        <div id="dashboard-wrapper">
-          <?php print render($page['content']['dashboard']); ?>
-        </div>
-      <?php endif; ?>
-
-      <?php print render($page['content']['system_main']); ?>
+      <?php print render($page['content']); ?>
     </div>
 
     <?php if (isset($page['sidebar_right'])): ?>
