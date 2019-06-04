922e8a58f627766bb5458420f09c349a514738f8
ossset <ossset@protonmail.com>
2019-06-04 14:23:40 +0300

    refactor: code refactor

---
+++ sites/all/themes/new_falanster/templates/node--static_page.tpl.php
@@ -0,0 +1,10 @@
+<?php if ($content['project_hot']): ?>
+        <div class="project_hot">
+            <img src="" alt="">
+        </div>
+<?php endif; ?>
+<?php if ($content['project_end']): ?>
+    <div class="project_end">
+        <img src="" alt="">
+    </div>
+<?php endif; ?>
