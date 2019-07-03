dfcb4459fc3310e47bd82d1ff2e321a929678eb7
koverchik <koverchik-work@yandex.ru>
2019-07-03 21:58:27 +0300

    content type blog in new_falanster theme

--- sites/all/themes/new_falanster/templates/node--article.tpl.php
+++ sites/all/themes/new_falanster/templates/node--article.tpl.php
@@ -1,28 +1,34 @@
 
 <main>
    <section class="banner">
-
      <div class="banner__photo" style="background-image: url('<?php $field_background = file_create_url($node->field_background_picture_event['und'][0]['uri']); print render($field_background)?>');">
-
-
        <div class="banner__container container-inner">
          <div class="banner__text">
            <h1 class="banner__heading"><?php print($title)?></h1>
-           <p class="banner__info">
-             <span class="banner__date">
-               <?php print($date)?>
-             </span>
-             <span class="banner__author">
-               <?php print($name)?>
-             </span>
-           </p>
+            <p class="banner__info">
+               <?php
+               $my_field_items = field_get_items('node', $node, 'field_publication_date');
+               $display_the_field = $my_field_items[0]['value'];
+                 if ($display_the_field == 1) {
+                   echo'<span class="banner__date">';
+                   print($date);
+                   echo'</span>';
+                }
+               ?>
+
+               <?php
+               $my_field_items = field_get_items('node', $node, 'field_publication_author');
+               $display_the_field = $my_field_items[0]['value'];
+                 if ($display_the_field == 1) {
+                echo'<span class="banner__author">';
+                print($name);
+                echo'</span>';}
+                ?>
+              </p>
          </div>
 
-         <?php
-
-        $my_field_items = field_get_items('node', $node, 'field_display_the_field');
+         <?php $my_field_items = field_get_items('node', $node, 'field_display_the_field');
         $display_the_field = $my_field_items[0]['value'];
-
         if ($display_the_field == 1) {
             echo '<p class="banner__date-block">';
             $field_field_date = field_get_items('node', $node, 'field_field_date');
@@ -33,10 +39,7 @@
             $day = substr($fied_data_event_rest, 8, 2);
             print_r($day .".".$month .".". $year. ", " . $time);
             echo '</p>';
-
-        }
-
-      ?>
+          }?>
 </div>
      </div>
    </section>
@@ -47,7 +50,6 @@
            <?php print render($content['body'])?>
          <img src="<?php $field_background = file_create_url($node->field_picture_epilog ['und'][0]['uri']); print render($field_background)?>" alt="" width="778" height="438">
        </article>
-       <a class="content__btn" href="https://falanster.by/be/volunteers">Стань валанцёрам!</a>
-     </div>
+      </div>
    </section>
  </main>
